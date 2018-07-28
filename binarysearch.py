6b7900db0e9560b01dc88a2e0d0db91ffd74c356
Guoquan <guoquan.xing@icloud.com>
2018-07-28 15:43:04 +0800

    correct binarysearch

--- binarysearch.py
+++ binarysearch.py
@@ -1,8 +1,9 @@
 def binarysearch(array, item):
     head = 0
     tail = len(array) -1
-    mid = (head + tail) // 2  #for python 3.X floor division
+
     while (head <= tail):
+        mid = (head + tail) // 2  # for python 3.X floor division
         if(array[mid] < item):
             head = mid + 1
         elif(array[mid] > item):
@@ -10,4 +11,9 @@ def binarysearch(array, item):
         else:
             return mid
     return None;
+
+
+array = [1,2,3,4,5,6,7,8,9]
+
+print(binarysearch(array, 6))    # expected output is 5
 
